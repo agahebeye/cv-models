1638ca91792563e8ea265da7ee005ad8abd75f1a
agahebeye <agahebeye@gmail.com>
2022-11-17 09:35:56 +0200

    GuestLayout.tsx

--- resources/ts/Layouts/GuestLayout.tsx
+++ resources/ts/Layouts/GuestLayout.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ApplicationLogo from '~/Components/ApplicationLogo';
-import { Link } from '@inertiajs/inertia-react';
+import React from "react";
+import ApplicationLogo from "~/Components/ApplicationLogo";
+import { Link } from "@inertiajs/inertia-react";
 
-export default function Guest({ children }) {
+export default function Guest({ header, children }: LayoutProps) {
     return (
         <div className="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0 dark:bg-gray-900">
             <div>
@@ -12,6 +12,13 @@ export default function Guest({ children }) {
             </div>
 
             <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md dark:bg-gray-800 sm:rounded-lg">
+                {header && (
+                    <header className="bg-white shadow dark:bg-gray-800">
+                        <div className="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                            {header}
+                        </div>
+                    </header>
+                )}
                 {children}
             </div>
         </div>
