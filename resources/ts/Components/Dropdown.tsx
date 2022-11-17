92b9970543a3d9401e6d5863e2bd2715c02a15ff
agahebeye <agahebeye@gmail.com>
2022-11-17 05:51:53 +0200

    typing Dropwdown.tsx

--- resources/ts/Components/Dropdown.tsx
+++ resources/ts/Components/Dropdown.tsx
@@ -1,11 +1,37 @@
-import React, { useState, useContext, Fragment } from 'react';
-import { Link } from '@inertiajs/inertia-react';
-import { Transition } from '@headlessui/react';
+import React, { useState, useContext, Fragment } from "react";
+import { Link } from "@inertiajs/inertia-react";
+import { Transition } from "@headlessui/react";
 
-const DropDownContext = React.createContext();
+interface IDropdownContext {
+    open: boolean;
+    setOpen?:React.Dispatch<React.SetStateAction<boolean>>;
+    toggleOpen?: () => void;
+}
 
-const Dropdown = ({ children }) => {
-    const [open, setOpen] = useState(false);
+interface IDropdownChildren {
+    children?: React.ReactNode;
+}
+
+interface IDropdownContenChildren extends IDropdownChildren {
+    align: string;
+    width: string;
+    contentClasses: string;
+}
+
+interface IDropdownLinkChildren extends IDropdownChildren {
+    href: string;
+    method?: string;
+    as?: string;
+}
+
+const defaultState = {
+    open: false,
+};
+
+const DropDownContext = React.createContext<IDropdownContext>(defaultState);
+
+const Dropdown = ({ children }: IDropdownChildren) => {
+    const [open, setOpen] = useState(defaultState.open);
 
     const toggleOpen = () => {
         setOpen((previousState) => !previousState);
@@ -18,33 +44,43 @@ const Dropdown = ({ children }) => {
     );
 };
 
-const Trigger = ({ children }) => {
+const Trigger = ({ children }: IDropdownChildren) => {
     const { open, setOpen, toggleOpen } = useContext(DropDownContext);
 
     return (
         <>
             <div onClick={toggleOpen}>{children}</div>
 
-            {open && <div className="fixed inset-0 z-40" onClick={() => setOpen(false)}></div>}
+            {open && (
+                <div
+                    className="fixed inset-0 z-40"
+                    onClick={() => setOpen?.(false)}
+                ></div>
+            )}
         </>
     );
 };
 
-const Content = ({ align = 'right', width = '48', contentClasses = 'py-1 bg-white dark:bg-gray-700', children }) => {
+const Content = ({
+    align = "right",
+    width = "48",
+    contentClasses = "py-1 bg-white dark:bg-gray-700",
+    children,
+}: Partial<IDropdownContenChildren>) => {
     const { open, setOpen } = useContext(DropDownContext);
 
-    let alignmentClasses = 'origin-top';
+    let alignmentClasses = "origin-top";
 
-    if (align === 'left') {
-        alignmentClasses = 'origin-top-left left-0';
-    } else if (align === 'right') {
-        alignmentClasses = 'origin-top-right right-0';
+    if (align === "left") {
+        alignmentClasses = "origin-top-left left-0";
+    } else if (align === "right") {
+        alignmentClasses = "origin-top-right right-0";
     }
 
-    let widthClasses = '';
+    let widthClasses = "";
 
-    if (width === '48') {
-        widthClasses = 'w-48';
+    if (width === "48") {
+        widthClasses = "w-48";
     }
 
     return (
@@ -61,22 +97,29 @@ const Content = ({ align = 'right', width = '48', contentClasses = 'py-1 bg-whit
             >
                 <div
                     className={`absolute z-50 mt-2 rounded-md shadow-lg ${alignmentClasses} ${widthClasses}`}
-                    onClick={() => setOpen(false)}
+                    onClick={() => setOpen?.(false)}
                 >
-                    <div className={`rounded-md ring-1 ring-black ring-opacity-5 ` + contentClasses}>{children}</div>
+                    <div
+                        className={
+                            `rounded-md ring-1 ring-black ring-opacity-5 ` +
+                            contentClasses
+                        }
+                    >
+                        {children}
+                    </div>
                 </div>
             </Transition>
         </>
     );
 };
 
-const DropdownLink = ({ href, method, as, children }) => {
+const DropdownLink = ({ href, method, as, children }: IDropdownLinkChildren) => {
     return (
         <Link
             href={href}
             method={method}
             as={as}
-            className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out"
+            className="block w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 transition duration-150 ease-in-out dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800"
         >
             {children}
         </Link>
